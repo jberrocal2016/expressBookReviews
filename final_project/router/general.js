0f74c9dca221eb6006dc51b2669b4926224326bc
jberrocal2016 <jberrocal2016@fau.edu>
2025-03-19 22:47:33 -0400

    completed GET book by title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -44,8 +44,16 @@ public_users.get("/author/:author", function (req, res) {
 
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const searchTitle = req.params.title.trim().toLocaleLowerCase(); // Normalize input
+  let booksByTitle = Object.values(books).filter(
+    (book) => book.title.toLocaleLowerCase().includes(searchTitle) // Case-insensitive & partial match
+  );
+
+  if (booksByTitle.length > 0) {
+    return res.status(200).json(booksByTitle);
+  } else {
+    return res.status(404).json({ message: "No books found with this title" });
+  }
 });
 
 //  Get book review
